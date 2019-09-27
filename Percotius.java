ce0c00ee002b1e455c4de45d2096f54e0d475a5e
Scott Graham <scott.graham@dsu.edu>
2019-09-27 00:56:42 -0500

    Initial commit

---
+++ Percotius.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Percotius {
+    public static void main(String[] args) {
+        System.out.println("This is a thing.");
+    }
+}
